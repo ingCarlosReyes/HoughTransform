1d87e2c6c5e5cbf27a47caaeeebc3506ce618868
ingCarlosReyes <ingcarlosareyes@gmail.com>
2022-10-03 21:50:56 -0500

    4rd fail

--- haughTransform.py
+++ haughTransform.py
@@ -1,11 +1,11 @@
-#non- vectorized
+#vectorized
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 
 
-def line_detection_non_vectorized(image, edge_image, num_rhos=180, num_thetas=180, t_count=220):
+def line_detection_vectorized(image, edge_image, num_rhos=180, num_thetas=180, t_count=220):
   edge_height, edge_width = edge_image.shape[:2]
   edge_height_half, edge_width_half = edge_height / 2, edge_width / 2
   #
@@ -31,35 +31,40 @@ def line_detection_non_vectorized(image, edge_image, num_rhos=180, num_thetas=18
   subplot4 = figure.add_subplot(1, 4, 4)
   subplot4.imshow(image)
   #
-  for y in range(edge_height):
-    for x in range(edge_width):
-      if edge_image[y][x] != 0:
-        edge_point = [y - edge_height_half, x - edge_width_half]
-        ys, xs = [], []
-        for theta_idx in range(len(thetas)):
-          rho = (edge_point[1] * cos_thetas[theta_idx]) + (edge_point[0] * sin_thetas[theta_idx])
-          theta = thetas[theta_idx]
-          rho_idx = np.argmin(np.abs(rhos - rho))
-          accumulator[rho_idx][theta_idx] += 1
-          ys.append(rho)
-          xs.append(theta)
-        subplot3.plot(xs, ys, color="white", alpha=0.05)
+  edge_points = np.argwhere(edge_image != 0)
+  edge_points = edge_points - np.array([[edge_height_half, edge_width_half]])
+  #
+  rho_values = np.matmul(edge_points, np.array([sin_thetas, cos_thetas]))
+  #
+  accumulator, theta_vals, rho_vals = np.histogram2d(
+      np.tile(thetas, rho_values.shape[0]),
+      rho_values.ravel(),
+      bins=[thetas, rhos]
+  )
+  accumulator = np.transpose(accumulator)
+  lines = np.argwhere(accumulator > t_count)
+  rho_idxs, theta_idxs = lines[:, 0], lines[:, 1]
+  r, t = rhos[rho_idxs], thetas[theta_idxs]
 
-  for y in range(accumulator.shape[0]):
-    for x in range(accumulator.shape[1]):
-      if accumulator[y][x] > t_count:
-        rho = rhos[y]
-        theta = thetas[x]
-        a = np.cos(np.deg2rad(theta))
-        b = np.sin(np.deg2rad(theta))
-        x0 = (a * rho) + edge_width_half
-        y0 = (b * rho) + edge_height_half
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
-        subplot3.plot([theta], [rho], marker='o', color="yellow")
-        subplot4.add_line(mlines.Line2D([x1, x2], [y1, y2]))
+  for ys in rho_values:
+    subplot3.plot(thetas, ys, color="white", alpha=0.05)
+
+  subplot3.plot([t], [r], color="yellow", marker='o')
+
+  for line in lines:
+    y, x = line
+    rho = rhos[y]
+    theta = thetas[x]
+    a = np.cos(np.deg2rad(theta))
+    b = np.sin(np.deg2rad(theta))
+    x0 = (a * rho) + edge_width_half
+    y0 = (b * rho) + edge_height_half
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    subplot3.plot([theta], [rho], marker='o', color="yellow")
+    subplot4.add_line(mlines.Line2D([x1, x2], [y1, y2]))
 
   subplot3.invert_yaxis()
   subplot3.invert_xaxis()
@@ -73,8 +78,8 @@ def line_detection_non_vectorized(image, edge_image, num_rhos=180, num_thetas=18
 
 
 if __name__ == "__main__":
-  for i in range(1):
-    image = cv2.imread(f"sample-{i}.png")
+  for i in range(3):
+    image = cv2.imread(f"sample-{i+1}.png")
     edge_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     edge_image = cv2.GaussianBlur(edge_image, (3, 3), 1)
     edge_image = cv2.Canny(edge_image, 100, 200)
@@ -88,4 +93,4 @@ if __name__ == "__main__":
         cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)),
         iterations=1
     )
-    line_detection_non_vectorized(image, edge_image)
+    line_detection_vectorized(image, edge_image)
